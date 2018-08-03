bc77748bceb7013c73d2d94c926f2d0bcd519819
Kate Preston <ktweeden@googlemail.com>
2018-08-03 20:05:47 +0100

    Isogram solution complete

--- c/isogram/src/isogram.c
+++ c/isogram/src/isogram.c
@@ -1,78 +1,21 @@
 #include "isogram.h"
-#include <stdio.h>
 #include <string.h>
-
-
 
 bool is_isogram(const char phrase[])
 {
-  int letters[26] = {0};
-  bool result = true;
+  bool letters[26] = { false };
   int length = strlen(phrase);
-  for(int i = 0; i < length; i++ )
-  {
+
+  for (int i = 0; i < length; i++) {
     int letter = phrase[i];
-    if(letter >= 97 && letter <= 122) 
-    {
-      if (letters[letter-97] == 1)
-      {
-        result = false;
-        break;
-      }
-      else
-      {
-        letters[letter-97] = 1;
-      }
-    }
-    else if(letter >= 65 && letter <= 90) 
-    {
-      if (letters[letter-65] == 1)
-      {
-        result = false;
-        break;
-      }
-      else
-      {
-        letters[letter-65] = 1;
-      }
-    }
+    int adjuster;
+
+    if (letter >= 97 && letter <= 122) { adjuster = 97; }
+    else if (letter >= 65 && letter <= 90) { adjuster = 65; }
+    else { continue; }
+
+    if (letters[letter - adjuster]) { return false; }
+    letters[letter - adjuster] = true;
   }
-  return result;
+  return true;
 }
-
-
-
-// bool is_isogram(const char phrase[])
-// {
-//   int length = strlen(phrase);
-//   char arr[length];
-//   bool result = true;
-  
-//   for(int i = 0; i < length; i++)
-//   {
-//     if(contains(phrase[i], arr))
-//     {
-//       result = false;
-//     }
-//     else 
-//     {
-//       arr[i] = phrase[i];
-//     }
-//   }
-//   return result;
-// }
-
-// bool contains(const char character, const char container[])
-// {
-//   int length = strlen(container);
-//   bool result = false;
-  
-//   for(int i = 0; i < length; i++)
-//   {
-//     if(character == container[i])
-//     {
-//       result = true;
-//     }
-//   }
-//   return result;
-// }
