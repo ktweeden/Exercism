3fa49abbf91bf2c49822be304246011f77d9ac74
ktweeden <ktweeden@googlemail.com>
2018-09-12 13:29:13 +0100

    Refactored acronym solution to use pointer arithmatic

--- c/acronym/src/acronym.c
+++ c/acronym/src/acronym.c
@@ -1,40 +1,30 @@
 #include "acronym.h"
 #include <ctype.h>
-#include <stdbool.h>
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 char *abbreviate(const char *phrase) {
-  if (phrase == NULL || strcmp(phrase, "") == 0) {
-    static char* null = NULL;
-    return null;
+  if (phrase == NULL || phrase[0] == '\0') {
+    return NULL;
   }
-  else {
-    const int length = strlen(phrase);
-    char* p = (char*) malloc(length);
 
-    int letter_count = 0;
-    
-    for (int i = 0; i < length; i++) {
-      if(i == 0) {
-        p[i] = phrase[i];
-        letter_count += 1;
-      } else if (isseparator(phrase[i])) {
-        p[letter_count] = phrase[i +1];
-        letter_count += 1;
-      }
+  const int length = strlen(phrase);
+  char* p = malloc(length);
+  char* first = p;
+  
+  for (int i = 0; i < length; i++) {
+    if(i == 0) {
+      *p = toupper(phrase[i]);
+      p++;
+    } else if (isseparator(phrase[i])) {
+      *p = toupper(phrase[i +1]);
+      p++;
+      i++;
     }
-
-    char* result = (char*) malloc(letter_count+1);
-    result[letter_count] = '\0'; 
-    for (int i = 0; i < letter_count; i++) {
-      result[i] = toupper(p[i]);
-    }
-    free(p);
-
-    return (char*) result;
   }
+
+  char* result = (char*) realloc(first, p-first);
+  return (char*) result;
 }
 
 bool isseparator(char c) {
